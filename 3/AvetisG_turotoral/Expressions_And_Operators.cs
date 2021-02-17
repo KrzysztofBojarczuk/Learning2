8d0722e7d9bb1b40724a33c2b10dd5223f28dd47
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-02-17 15:46:49 +0100

    Create Expressions_And_Operators.cs

---
+++ 3/AvetisG_turotoral/Expressions_And_Operators.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal simpleExpression1 = 5 - 10;
+            decimal simpleExpression2 = 4 * 10;
+            decimal complexExpression = (((6 * 7) - 4) + simpleExpression1)/simpleExpression2;
+            Console.WriteLine(simpleExpression1);
+            Console.WriteLine(simpleExpression2);
+            Console.WriteLine(complexExpression);
+        }
+    }
+}
