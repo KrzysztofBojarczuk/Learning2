1364d24320f1619bbf60c008ec49e8fc2478184c
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-02-18 15:31:03 +0100

    Create while.cs

---
+++ 3/AvetisG_turotoral/while.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+
+            while (index < 10)
+            {
+                Console.WriteLine("The number is {0}",index);
+                index = index + 1;
+            }
+        }
+    }   
+
+}
