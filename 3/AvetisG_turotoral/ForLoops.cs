ac00fc55d6a02ef0f05ff053d57aaaa1a79756ec
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-02-20 12:52:59 +0100

    Create ForLoops.cs

---
+++ 3/AvetisG_turotoral/ForLoops.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ConsoleApp11ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                
+                Console.WriteLine("The i is {0}", i);
+            }
+        }
+    }
+}
